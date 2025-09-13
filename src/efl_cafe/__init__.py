6c7c2c52fca806b6059032f99d88b352aa0de8c7
aerladis <deniz.akar123@gmail.com>
2025-09-13 15:49:51 +0300

    Delete src directory

--- src/efl_cafe/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-EFL Cafe - English as a Foreign Language Question Generator
-
-A PyQt5 application for generating educational questions with AI integration.
-"""
-
-__version__ = "1.0.0"
-__author__ = "EFL Cafe Team"
